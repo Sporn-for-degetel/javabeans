7fc3d5038c0b9df1e4054cc25e9a9cf673263b87
Sporn pour Degetel <62709077+Sporn-for-degetel@users.noreply.github.com>
2020-07-30 15:30:39 +0200

    Revert "prepare release : version, javadoc, readme"
    This reverts commit 94661aeef8bc217018dc3ff2b90f3fbf40aefe75.

--- sporniket-javabeans-doclet/src/main/java/com/sporniket/libre/javabeans/doclet/basic/BasicGeneratorBase.java
+++ sporniket-javabeans-doclet/src/main/java/com/sporniket/libre/javabeans/doclet/basic/BasicGeneratorBase.java
@@ -33,7 +33,7 @@ import com.sporniket.libre.javabeans.doclet.codespecs.ClassSpecs;
  * <hr>
  *
  * @author David SPORN
- * @version 20.05.00
+ * @version 20.04.04
  * @since 17.09.00
  */
 public class BasicGeneratorBase
